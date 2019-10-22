d8fdb3399f62630ab4cbeb7dcfe3bab6c781fbe1
Brittney Haselmayer <brittneyhaselmayer@yahoo.com>
2019-10-22 17:38:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each do |key, value|
+  data.each do |x, value|
     value.each do |new_value, names|
       names.each do |name|
 
@@ -8,11 +8,11 @@ def nyc_pigeon_organizer(data)
           new_hash[name] = {}
         end
 
-        if !new_hash[name][key]
-          new_hash[name][key] = []
+        if !new_hash[name][x]
+          new_hash[name][x] = []
         end
 
-        new_hash[name][key] << new_value.to_s
+        new_hash[name][x] << new_value.to_s
 
       end
     end
